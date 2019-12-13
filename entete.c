34e58ffc332396df1540fb343dce3f14ac449a3f
Mathis Perrier <mathis.perrier73@gmail.com>
2019-12-13 11:44:34 +0100

    1.1 fini TODO : Mise en page

--- entete.c
+++ entete.c
@@ -23,7 +23,86 @@ int main(int argc, char *argv[]) {
 
       // check so its really an elf file
       if (memcmp(header.e_ident, ELFMAG, SELFMAG) == 0) {
-         printf("VALIDE");
+        printf("En-tête ELF:\n  Magique:\t");
+        for (int i = 0; i < 16; i++)
+          printf("%02x ",header.e_ident[i]);
+
+        printf("\n  Classe:\t");
+        switch (header.e_ident[EI_CLASS]) {
+          case ELFCLASS32:
+            printf("ELF32");
+          break;
+          case ELFCLASS64:
+            printf("ELF64");
+          break;
+          default://ELFCLASSNONE
+            printf("Invalide");
+        }
+
+        printf("\n  Données:\t");
+        switch (header.e_ident[EI_DATA]) {
+          case ELFDATA2LSB:
+            printf("Complément à deux, petit boutien (little endians)");
+          break;
+          case ELFDATA2MSB:
+            printf("Complément à deux, gros boutien (big endians)");
+          break;
+          default://ELFDATANONE
+            printf("Invalide");
+        }
+
+        printf("\n  Version:\t");
+        switch (header.e_ident[EI_VERSION]) {
+          case EV_CURRENT:
+            printf("1 (current)");
+          break;
+          default://EV_NONE
+            printf("Invalide");
+        }
+
+        printf("\n  OS/ABI:\t");
+        switch (header.e_ident[EI_OSABI]) {
+          case ELFOSABI_HPUX:
+            printf("HP-UX");
+          break;
+          case ELFOSABI_NETBSD:
+            printf("NetBSD");
+          break;
+          case ELFOSABI_LINUX:
+            printf("Linux");
+          break;
+          case ELFOSABI_SOLARIS:
+            printf("Solaris");
+          break;
+          case ELFOSABI_IRIX:
+            printf("IRIX");
+          break;
+          case ELFOSABI_FREEBSD:
+            printf("FreeBSD");
+          break;
+          case ELFOSABI_TRU64:
+            printf("UNIX TRU64");
+          break;
+          case ELFOSABI_ARM:
+            printf("Architecture ARM");
+          break;
+          case ELFOSABI_STANDALONE:
+            printf("autonome (intégrée)");
+          break;
+          default://ELFOSABI_NONE ou ELFOSABI_SYSV
+            printf("UNIX System V");
+        }
+        printf("\n  Version ABI:\t0x%01x",header.e_ident[EI_ABIVERSION]);
+        printf("\n  Adresse du point d'entrée:\t0x%01x", header.e_ident[EI_PAD]);
+        printf("\n  Début des en-tête de programme:\t%lu", header.e_phoff);
+        printf("\n  Début des en-tête de section:\t%lu", header.e_shoff);
+        printf("\n  Fanions:\t0x%01x", header.e_flags);
+        printf("\n  Taille de cet en-tête:\t%d (octets)", header.e_ehsize);
+        printf("\n  Taille de l'en-tête du programme:\t%d (octets)", header.e_phentsize);
+        printf("\n  Nombre d'en-tête du programme:\t%d", header.e_phnum);
+        printf("\n  Taille des en-têtes de section:\t%d (octets)", header.e_shentsize);
+        printf("\n  Nombre d'en-têtes de section:\t%d", header.e_shnum);
+        printf("\n  Table d'indexes des chaînes d'en-tête de section:\t%d\n", header.e_shstrndx);
       }
       fclose(elfFile);
     }
