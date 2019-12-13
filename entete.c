8e53b4aedf7c85e3b184691840402fca1b034233
Mathis Perrier <mathis.perrier73@gmail.com>
2019-12-13 12:15:12 +0100

    Il manquait des bouts de l'entête Il manque des machines sinon

--- entete.c
+++ entete.c
@@ -92,7 +92,91 @@ int main(int argc, char *argv[]) {
           default://ELFOSABI_NONE ou ELFOSABI_SYSV
             printf("UNIX System V");
         }
-        printf("\n  Version ABI:\t\t\t\t0x%01x",header.e_ident[EI_ABIVERSION]);
+        printf("\n  Version ABI:\t\t\t\t%d",header.e_ident[EI_ABIVERSION]);
+        printf("\n  Type:\t\t\t\t");
+        switch (header.e_type) {
+          case ET_REL:
+            printf("REL (Fichier repositionnable)");
+          break;
+          case ET_EXEC:
+            printf("EXEC (Fichier executable)");
+          break;
+          case ET_DYN:
+            printf("DYN (Objet pratagé)");
+          break;
+          case ET_CORE:
+            printf("CORE (Fichier core)");
+          break;
+          default://ET_NONE
+            printf("Type inconnu");
+        }
+        printf("\n  Machine:\t\t\t\t\t");
+        switch (header.e_machine) {
+          case EM_M32:
+            printf("WE 32100 AT&T");
+          break;
+          case EM_SPARC:
+            printf("SPARC Sun Microsystems");
+          break;
+          case EM_386:
+            printf("80386 Intel");
+          break;
+          case EM_68K:
+            printf("68000 Motorola");
+          break;
+          case EM_88K:
+            printf("88000 Motorola");
+          break;
+          case EM_860:
+            printf("80860 Intel");
+          break;
+          case EM_MIPS:
+            printf("RS3000 MIPS (uniquement gros boutien)");
+          break;
+          case EM_PARISC:
+            printf("HP/PA");
+          break;
+          case EM_SPARC32PLUS:
+            printf("SPARC avec jeu d'instructions amélioré");
+          break;
+          case EM_PPC:
+            printf("PowerPC");
+          break;
+          case EM_PPC64:
+            printf("PowerPC 64 bits");
+          break;
+          case EM_S390:
+            printf("S/390 IBM");
+          break;
+          case EM_ARM:
+            printf("ARM");
+          break;
+          case EM_SH:
+            printf("SuperH de Renesas");
+          break;
+          case EM_SPARCV9:
+            printf("SPARC v9 64 bits");
+          break;
+          case EM_IA_64:
+            printf("Itanium Intel");
+          break;
+          case EM_X86_64:
+            printf("x86-64 AMD");
+          break;
+          case EM_VAX:
+            printf("Vax DEC");
+          break;
+          default://EM_NONE
+            printf("Machine inconnue");
+        }
+        printf("\n  Version:\t\t\t\t0x%01x",header.e_ident[EI_ABIVERSION]);
+        switch (header.e_ident[EI_VERSION]) {
+          case EV_CURRENT:
+            printf("0x1");
+          break;
+          default://EV_NONE
+            printf("Invalide");
+        }
         printf("\n  Adresse du point d'entrée:\t\t0x%01x", header.e_ident[EI_PAD]);
         printf("\n  Début des en-tête de programme:\t%lu", header.e_phoff);
         printf("\n  Début des en-tête de section:\t\t%lu", header.e_shoff);
