0dd15e090ca2582537a51ba69504ae5b64aaf33f
Mathis Perrier <mathis.perrier73@gmail.com>
2019-12-15 10:08:10 +0100

    Début du travail. À compiler voir si ça marche

--- tableSections.c
+++ tableSections.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[]) {
   FILE * elfFile;
 
   Elf64_Ehdr header;
+  Elf64_Shdr section;
 
   char buff[255];
   if (argc != 2) {
@@ -24,7 +25,23 @@ int main(int argc, char *argv[]) {
 
       // check so its really an elf file
       if (memcmp(header.e_ident, ELFMAG, SELFMAG) == 0) {
+        //SectNames = malloc(sectHdr.sh_size);
+        //fseek(ElfFile, sectHdr.sh_offset, SEEK_SET);
+        //fread(SectNames, 1, sectHdr.sh_size, ElfFile);
 
+        // read all section headers
+        for (int i = 0; i < header.e_shnum; i++)
+          {
+          const char* name = "";
+
+          fseek(elfFile, header.e_shoff + i * sizeof(section), SEEK_SET);
+          fread(&section, 1, sizeof(section), elfFile);
+
+          // print section name
+          //if (sectHdr.sh_name);
+          //  name = SectNames + sectHdr.sh_name;
+          printf("%2u %s\n", i, section.sh_name);
+        }
       }
       fclose(elfFile);
     }
