9a52b0336cbdb1b8f61c931ef8f695610cf92025
ruscot <thonymartiinez@gmail.com>
2019-12-21 13:43:21 +0100

    automatisation des tests, fichier main principal, il manque juste l'enlevement des byte_shift

---
+++ Etape1/etape1-Principal.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <elf.h>
+#include "../librairie/lib-bigEndian.c"
+#include "etape1-1.c"
+#include "etape1-2.c"
+#include "etape1-3.c"
+#include "etape1-4.c"
+#include "etape1-5.c"
+
+/* Contient la fonction main qui se chargera d'appeler les 
+ * fonctions voulues
+*/
