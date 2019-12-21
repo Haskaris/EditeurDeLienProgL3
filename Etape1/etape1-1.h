6d79c312bf243ba15fea6e1760483ca0dae6d62b
Mathis Perrier <mathis.perrier73@gmail.com>
2019-12-21 20:59:34 +0100

    Ajout d'un ifndef

--- Etape1/etape1-1.h
+++ Etape1/etape1-1.h
@@ -1,3 +1,5 @@
+#ifndef __ETAPE1_1_H__
+#define __ETAPE1_1_H__
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,49 +7,51 @@
 #include "../librairie/lib-bigEndian.h"
 
 /* Affiche la classe du fichier ELF, 32 ou 64 bits
- * arguments : 
+ * arguments :
  *	- l'entête du fichier ELF (h)
 */
 void classeArchitecture(Elf32_Ehdr h);
 
 /* Affiche l'encodage du fichier ELF, little ou big ENDIAN
- * arguments : 
+ * arguments :
  *	- l'entête du fichier ELF (h)
 */
 void encodage(Elf32_Ehdr h);
 
 /* Affiche le numéro de version de la spécification du format ELF
- * arguments : 
+ * arguments :
  *	- l'entête du fichier ELF (h)
  *	- précise si on affiche l'adresse ou non (addressMode)
 */
 void fileVersion(Elf32_Ehdr h, int addressMode);
 
 
-/* Affiche le système d'exploitation et l'interface 
+/* Affiche le système d'exploitation et l'interface
  * binaire des applications (ABI)
- * arguments : 
+ * arguments :
  *	- l'entête du fichier ELF (h)
 */
 void osAbi(Elf32_Ehdr h);
 
 /* Affiche le type du fichier objet
- * arguments : 
+ * arguments :
  *	- l'entête du fichier ELF (h)
  *	- entier pour savoir si on est en bigENDIAN ou en litleENDIAN (bigEndian)
 */
 void fileType(Elf32_Ehdr h, int bigEndian);
 
 /* Affiche l'architecture nécessaire à un fichier individuel
- * arguments : 
+ * arguments :
  *	- l'entête du fichier ELF (h)
  *	- entier pour savoir si on est en bigENDIAN ou en litleENDIAN (bigEndian)
 */
 void machine(Elf32_Ehdr h, int bigEndian);
 
 /* Affiche l'entête d'un fichier ELF
- * arguments : 
+ * arguments :
  *	- l'entête du fichier ELF (h)
  *	- entier pour savoir si on est en bigENDIAN ou en litleENDIAN (bigEndian)
 */
 void affichage_Entete_Fichier_ELF(Elf32_Ehdr header, int bigEndian);
+
+#endif
