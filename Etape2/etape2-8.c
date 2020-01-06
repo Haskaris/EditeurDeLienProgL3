f82655f095ac619336e41e596b6050530fd8ab8c
DrEdwinn <martinsenn@orange.fr>
2020-01-06 09:09:19 +0100

    travail nul en cours

--- Etape2/etape2-8.c
+++ Etape2/etape2-8.c
@@ -2,103 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <elf.h>
-
-int isbigendian(){
-    return 1;
-}
-
-uint16_t byteshift16(uint16_t n){
-    if (isbigendian()){
-        return ((n>>8)&0xff) | ((n<<8)&0xff00);
-    }
-    else{
-        return n;
-    }
-}
-
-uint32_t byteshift32(uint32_t n) {
-    if (isbigendian()){
-          return ((n>>24)&0xff) | ((n<<8)&0xff0000) | ((n>>8)&0xff00) | ((n<<24)&0xff000000);
-    }
-    else{
-        return n;
-    }
-}
+#include "../librairie/utils.h"
+#include "../librairie/lib-bigEndian.h"
+#include "../librairie/macro_section.h"
+#include "../librairie/macro_symbole.h"
+#include "../librairie/macro_header.h"
+#include "../librairie/macro_repositionnement.h"
 
 
-void get_section_name(FILE* elfFile,Elf32_Ehdr header,Elf32_Shdr section, char* name){
-    Elf32_Shdr table_chaine;
-    fseek(elfFile,byteshift32(header.e_shoff)+byteshift16(header.e_shstrndx)*byteshift16(header.e_shentsize),SEEK_SET);
-    fread(&table_chaine, 1, sizeof(Elf32_Shdr), elfFile);
-    fseek(elfFile,byteshift32(table_chaine.sh_offset)+byteshift32(section.sh_name),SEEK_SET);
-    char c=fgetc(elfFile);
-    int i=0;
-    while(c!='\0'){
-        name[i]=c;
-        i++;
-        c=fgetc(elfFile);
-    }
-    name[i]='\0';
+int fusion_reimplementation(FILE* elfFile1, FILE* elfFile2,FILE* output){
+	FILE * tempFile;
+	uint32_t section_name=0;
 
-}
+	Elf32_Ehdr header1,header2,headerOutput;
+	Elf32_Shdr section1,section2,sectionOut;
 
-
-int main(int argc, char* argv[]){
-    FILE * elfFile1;
-    FILE * elfFile2;
-    FILE * outputFile;
-    FILE * tempFile;
-    uint32_t section_name=0;
-
-    Elf32_Ehdr header1,header2,headerOutput;
-    Elf32_Shdr section1,section2,sectionOut;
-
-    char nom_section1[255],nom_section2[255];
-    if (argc != 4) {
-      printf("Utilisation : %s <ELF_FILE1> <ELF_FILE2> <OUTPUT_FILE>\n", argv[0]);
-      exit(1);
-    }
-    else {
-      elfFile1 = fopen(argv[1], "r");
-      elfFile2 = fopen(argv[2], "r");
-      outputFile = fopen(argv[3], "w");
-      tempFile = fopen("tempFileSectionTable","w");
-      if (elfFile1 == NULL || elfFile2==NULL || outputFile==NULL) {
-        printf("Erreur d'ouverture d'un fichier.\n");
-      }
-      else {
-          fread(&header1, sizeof(header1),1, elfFile1);
-          fread(&header2, sizeof(header2),1, elfFile2);
-          // check so its really an elf file
-          if (memcmp(header1.e_ident, ELFMAG, SELFMAG) == 0 && memcmp(header2.e_ident, ELFMAG, SELFMAG) == 0 ) {
-              fwrite(&header1,sizeof(header1),1,outputFile);
-              for (int i=0; i<byteshift16(header1.e_shnum);i++){ //parcours des sections du fichier 1
-                  fseek(elfFile1, byteshift32(header1.e_shoff) + i * byteshift16(header1.e_shentsize), SEEK_SET);
-                  fread(&section1, 1, sizeof(section2), elfFile1);
-                  get_section_name(elfFile1,header1,section1,nom_section1);
-                  for (int j=0;j<byteshift16(header2.e_shnum);j++){ //parcours des sections du fichier 2
-                      fseek(elfFile2, byteshift32(header2.e_shoff) + j * byteshift16(header2.e_shentsize), SEEK_SET);
-                      fread(&section2, 1, sizeof(section2), elfFile2);
-                      get_section_name(elfFile2,header2,section2,nom_section2);
-                      if(!strcmp(nom_section1,nom_section2)){ //si les sections ont le meme nom
-                          fprintf(tempFile,"%s",nom_section1);
-                          sectionOut.sh_name=section_name;
-                          sectionOut.sh_type=section1.sh_type;
-                          sectionOut.sh_flags=section1.sh_flags;
-                          sectionOut.sh_addr=section1.sh_addr;
-                          sectionOut.sh_offset=ftell(outputFile);
-                          sectionOut.sh_size=section1.sh_size+section2.sh_size;
-                          
-                      }
-                  }
-              }
-          }
-      }
-      fclose(elfFile1);
-      fclose(elfFile2);
-      fclose(outputFile);
-      fclose(tempFile);
-      //remove("tempFileSectionTable");
-  }
-
+	char nom_section1[255],nom_section2[255];
+		elfFile1 = fopen(argv[1], "r");
+		elfFile2 = fopen(argv[2], "r");
+		outputFile = fopen(argv[3], "w");
+		tempFile = fopen("tempFileSectionTable","w");
+		if (elfFile1 == NULL || elfFile2==NULL || outputFile==NULL) {
+		printf("Erreur d'ouverture d'un fichier.\n");
+		}
+		else {
+			fread(&header1, sizeof(header1),1, elfFile1);
+			fread(&header2, sizeof(header2),1, elfFile2);
+			// check so its really an elf file
+			if (memcmp(header1.e_ident, ELFMAG, SELFMAG) == 0 && memcmp(header2.e_ident, ELFMAG, SELFMAG) == 0 ) {
+				fwrite(&header1,sizeof(header1),1,outputFile);
+				for (int i=0; i<header1.e_shnum);i++){ //parcours des sections du fichier 1
+					fseek(elfFile1, header1.e_shoff) + i * header1.e_shentsize, SEEK_SET);
+					fread(&section1, 1, sizeof(section2), elfFile1);
+					get_section_name(elfFile1,header1,section1,nom_section1);
+					for (int j=0;j<header2.e_shnum;j++){ //parcours des sections du fichier 2
+						fseek(elfFile2, header2.e_shoff + j * header2.e_shentsize, SEEK_SET);
+						fread(&section2, 1, sizeof(section2), elfFile2);
+						get_section_name(elfFile2,header2,section2,nom_section2);
+						if(!strcmp(nom_section1,nom_section2)){ //si les sections ont le meme nom
+							fprintf(tempFile,"%s",nom_section1);
+							sectionOut.sh_name=section_name;
+							sectionOut.sh_type=section1.sh_type;
+							sectionOut.sh_flags=section1.sh_flags;
+							sectionOut.sh_addr=section1.sh_addr;
+							sectionOut.sh_offset=ftell(outputFile);
+							sectionOut.sh_size=section1.sh_size+section2.sh_size;
+						}
+					}
+				}
+			}
+		}
+	fclose(tempFile);
+	//remove("tempFileSectionTable");
 }
